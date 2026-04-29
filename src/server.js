4050ba8a06c5e9fbb3f5140aa740971cd2c327e3
faraulia <frhkzara@gmail.com>
2026-04-29 13:50:17 +0700

    initial commit

---
+++ src/server.js
@@ -0,0 +1,22 @@
+// console.log('Hallo kita akan membuat RESTful API');
+
+import express from 'express';
+import routes from './routes.js';
+
+// cross origin resource sharing
+import cors from 'cors';
+
+const app = express();
+const port = process.env.PORT || 3000;
+const host = process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0';
+
+app.use(cors({
+  origin: '*'
+}));
+
+app.use(express.json());
+app.use('/', routes);
+
+app.listen(port, () => {
+ console.log(`Server running at http://${host}:${port}`);
+}); 
